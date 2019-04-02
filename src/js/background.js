3f3a2d8194ce1957912b096802e95ecb04ce908d
Evan Moon <bboydart91@gmail.com>
2019-04-03 00:38:26 +0900

    background.js 내 스코프 생성

--- src/js/background.js
+++ src/js/background.js
@@ -1,31 +1,35 @@
 import '../img/icon-128.png';
 import '../img/icon-34.png';
 
-let workTimeData = {};
-
 function checkForValidUrl(tabId, changeInfo, tab) {
   chrome.pageAction.hide(tabId);
   if (tab.url.indexOf('https://ezhuman.bizmeka.com/product/golvwkmng/officegolvwkqry') === 0) {
     chrome.pageAction.show(tabId);
   }
-};
-
+}
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
 
-chrome.extension.onConnect.addListener(port => {
-  port.onMessage.addListener(msg => {
-    if (msg.type === 'fetchWorkTime') {
-      port.postMessage({
-        type: msg.type,
-        data: workTimeData,
-      });
+(function () {
+  let workTimeData = {};
+  // popup message
+  chrome.extension.onConnect.addListener(port => {
+    port.onMessage.addListener(msg => {
+      if (msg.type === 'fetchWorkTime') {
+        port.postMessage({
+          type: msg.type,
+          data: workTimeData,
+        });
+      }
+    });
+  });
+
+// content message
+  chrome.runtime.onMessage.addListener((msg) => {
+    if (msg.type === 'updateWorkTime') {
+      console.log('Received WorkTime', msg.data);
+      workTimeData = msg.data;
     }
   });
-});
+})();
 
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.type === 'onLoadWorkTimes') {
-    workTimeData = msg.data;
-  }
-});
 
