4415686f3299f6bfd49a1ff61dc415b43c034b80
Evan Moon <bboydart91@gmail.com>
2019-05-06 14:10:17 +0900

    bizmekaXMLParser 추가

---
+++ src/js/lib/bizmekaXMLParser.js
@@ -0,0 +1,73 @@
+/**
+ * @function bizmekaXMLParser
+ * @desc 비즈메카 XML API 응답을 파싱
+ */
+const BIZ_WORK_TYPES = {
+  HOLIDAY_TYPE: { bizmeka: '전일/반일', eng: 'holidayType' },
+  START_DATE: { bizmeka: '시작일', eng: 'startAt' },
+  END_DATE: { bizmeka: '종료일', eng: 'endAt' },
+  START_WORK_TIME: { bizmeka: '출근시각', eng: 'startAt' },
+  END_WORK_TIME: { bizmeka: '퇴근시각', eng: 'endAt' },
+  WORK_TIME: { bizmeka: '근무시간', eng: 'workTime' },
+};
+const WORK_TYPE = {
+  HOLIDAY: 'HOLIDAY',
+  WORK: 'WORK',
+};
+
+export function bizmekaXMLParser (xml) {
+  if (typeof xml !== 'string') {
+    throw new TypeError('Unexpected type of xml', 'bizmekaXMLParser');
+  }
+  const parser = new DOMParser();
+  const xmlDoc = parser.parseFromString(xml, 'text/xml');
+  const workdays = [];
+
+  for (let i = 0; i < xmlDoc.getElementsByTagName('userdata').length; i++) {
+    const string = xmlDoc.getElementsByTagName('userdata')[i].innerHTML;
+    if (!string || string.includes('CDATA')) {
+      continue;
+    }
+
+    /**
+     * @desc 문자열 안에 "전일/반일" 글자가 들어가있으면 휴가임
+     */
+    let workType = WORK_TYPE.WORK;
+    if (string.includes(BIZ_WORK_TYPES.HOLIDAY_TYPE.bizmeka)) {
+      workType = WORK_TYPE.HOLIDAY;
+    }
+
+    const result = { type: workType };
+    const parsed = string.trim().split(/\n/);
+    parsed.forEach(el => {
+      const data = el.replace(/○/gi, '').split(' : ');
+      const key = data[0].trim();
+      const value = data[1].trim();
+
+      switch (key) {
+        case BIZ_WORK_TYPES.HOLIDAY_TYPE.bizmeka:
+          result[BIZ_WORK_TYPES.HOLIDAY_TYPE.eng] = value;
+          break;
+        case BIZ_WORK_TYPES.START_DATE.bizmeka:
+          result[BIZ_WORK_TYPES.START_DATE.eng] = value;
+          break;
+        case BIZ_WORK_TYPES.END_DATE.bizmeka:
+          result[BIZ_WORK_TYPES.END_DATE.eng] = value;
+          break;
+        case BIZ_WORK_TYPES.START_WORK_TIME.bizmeka:
+          result[BIZ_WORK_TYPES.START_WORK_TIME.eng] = value;
+          break;
+        case BIZ_WORK_TYPES.END_WORK_TIME.bizmeka:
+          result[BIZ_WORK_TYPES.END_WORK_TIME.eng] = value;
+          break;
+        case BIZ_WORK_TYPES.WORK_TIME.bizmeka:
+          result[BIZ_WORK_TYPES.WORK_TIME.eng] = value;
+          break;
+      }
+    });
+
+    workdays.push(result);
+  }
+
+  return workdays;
+}
